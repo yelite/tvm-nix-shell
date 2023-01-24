065b77c86e14c4648677b78f3371995096addb08
Lite Ye <yelite958@gmail.com>
2023-01-23 23:41:39 -0500

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -36,29 +36,15 @@
         pkgs = channels.nixpkgs;
         inherit (pkgs) stdenv lib;
         useCuda = stdenv.isLinux;
+
         cudatoolkit = pkgs.cudaPackages_11_7.cudatoolkit;
-        cudaEnv = lib.optionalString useCuda ''
-          export CUDA_HOME="${cudatoolkit}"
-          # This is for NixOS, to add libcuda to ld path.
-          # TODO: condition this on the os type
-          export LD_LIBRARY_PATH="/var/run/opengl-driver/lib:$LD_LIBRARY_PATH"
-        '';
-        llvmPackages = pkgs.llvmPackages_11;
-        # Wrap mold according the snippet from https://github.com/NixOS/nixpkgs/pull/172452#issuecomment-1335903570
-        mold' = (pkgs.wrapBintoolsWith {
-          bintools = pkgs.mold;
-        }).overrideAttrs (old: {
-          installPhase = old.installPhase + ''
-            for variant in ld.mold ld64.mold; do
-              local underlying=$ldPath/$variant
-              [[ -e "$underlying" ]] || continue
-              wrap $variant ${pkgs.path}/pkgs/build-support/bintools-wrapper/ld-wrapper.sh $underlying
-            done
-          '';
-        });
+        tvm-llvm = pkgs.llvmPackages_11;
+        python = pkgs.python310;
+
       in
       {
         packages = utils.lib.exportPackages self.overlays channels;
+
         devShell = pkgs.mkShell
           {
             name = "tvm-shell";
@@ -66,90 +52,96 @@
             nativeBuildInputs = with pkgs; [
               pkg-config
               cmake
-              clang
               ccache
               cargo
               rustc
             ] ++ lib.optionals stdenv.isLinux [
-              # TODO: Remove this after mold can be built on macOS
-              mold'
+              mold-wrapped
+            ] ++ lib.optionals stdenv.isDarwin [
+              llvmPackages_14.bintools
             ];
 
             buildInputs = with pkgs; [
-              llvmPackages.llvm
-              llvmPackages.libclang
+              tvm-llvm.llvm
+              tvm-llvm.libclang
               openssl.dev
-              gtest
-            ] ++ lib.optionals stdenv.isLinux [
-              # elftoolchain
               (libbacktrace.override {
                 enableStatic = true;
               })
+              gtest
+            ] ++ lib.optionals stdenv.isLinux [
+              # elftoolchain
             ];
-            packages = with pkgs; [
-              python39
-              gdb
+
+            packages = with pkgs; ([
+              python
               ninja
               rustfmt
               clippy
               rust-analyzer
               doxygen
               clang-tools # To get the latest clangd
-              libclang
               git-lfs # for benchmark
             ]
-            ++ (with pkgs.python39Packages;
-              [
-                pip
-                setuptools
-                numpy
-                decorator
-                attrs
-                tornado
-                psutil
-                xgboost
-                cloudpickle
-                pytest
-                pillow
-                ipython
-                jupyter
-                opencv4 # For benchmark yolov3
-                seaborn # For benchmark analysis
-
-                pytorch-bin
-                torchvision
-
-                black
-                mypy
-                flake8
-              ])
             ++ lib.optionals useCuda [
               cudatoolkit
             ]
-            ++ lib.optionals (!pkgs.stdenv.isAarch64) (with pkgs;
-              [
-                wasmtime
-                wabt
-              ]);
+            ++ lib.optionals stdenv.isLinux [
+              gdb
+            ]
+            ++ lib.optionals (!pkgs.stdenv.isAarch64) [
+              wasmtime
+              wabt
+            ]
+            ++ (with python.pkgs; [
+              pip
+              setuptools
+              numpy
+              decorator
+              attrs
+              tornado
+              psutil
+              xgboost
+              cloudpickle
+              pytest
+              pillow
+              ipython
+
+              black
+              mypy
+              flake8
+            ]
+            ++ lib.optional stdenv.isLinux [
+              jupyter
+              opencv4 # For benchmark yolov3
+              seaborn # For benchmark analysis
+
+              pytorch-bin
+              torchvision
+            ])
+            );
 
             hardeningDisable = [ "fortify" ];
 
             shellHook = ''
               export TVM_HOME=$(pwd)/tvm
-              export PIP_PREFIX=$(pwd)/_build/pip_packages
-              export PYTHONPATH="$(pwd)/synr:$TVM_HOME/python:$PIP_PREFIX/${pkgs.python39.sitePackages}:$PYTHONPATH"
+              export PYTHONPATH="$(pwd)/synr:$TVM_HOME/python:$PYTHONPATH"
               export PATH="$PIP_PREFIX/bin:$PATH"
               unset SOURCE_DATE_EPOCH
 
-              export LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib"
+              export LIBCLANG_PATH="${tvm-llvm.libclang.lib}/lib"
 
-              export BINDGEN_EXTRA_CLANG_ARGS="-isystem ${llvmPackages.clang}/resource-root/include $NIX_CFLAGS_COMPILE"
-              export LLVM_AR="${llvmPackages.llvm}/bin/llvm-ar"
-
-              ${cudaEnv}
+              export BINDGEN_EXTRA_CLANG_ARGS="-isystem ${tvm-llvm.clang}/resource-root/include $NIX_CFLAGS_COMPILE"
+              export LLVM_AR="${tvm-llvm.llvm}/bin/llvm-ar"
 
               # Make sure we get the latest clangd
               export PATH="${pkgs.clang-tools}/bin:$PATH";
+            ''
+            + lib.optionalString useCuda ''
+              export CUDA_HOME="${cudatoolkit}"
+              # This is for NixOS, to add libcuda to ld path.
+              # TODO: condition this on the os type
+              export LD_LIBRARY_PATH="/var/run/opengl-driver/lib:$LD_LIBRARY_PATH"
             '';
           };
       };
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,20 @@ final: prev:
 {
   elftoolchain = prev.callPackage ./elftoolchain.nix { };
 
-  python39 = prev.python39.override {
+  # Wrap mold according the snippet from https://github.com/NixOS/nixpkgs/pull/172452#issuecomment-1335903570
+  mold-wrapped = (prev.wrapBintoolsWith {
+    bintools = prev.mold;
+  }).overrideAttrs (old: {
+    installPhase = old.installPhase + ''
+      for variant in ld.mold ld64.mold; do
+        local underlying=$ldPath/$variant
+        [[ -e "$underlying" ]] || continue
+        wrap $variant ${prev.path}/pkgs/build-support/bintools-wrapper/ld-wrapper.sh $underlying
+      done
+    '';
+  });
+
+  python310 = prev.python310.override {
     packageOverrides = python-self: python-super:
       let
         inherit (prev.lib) optionalString;
