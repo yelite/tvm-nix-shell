0df9bbccb3d69bf55862849d781cac422b98c751
Lite Ye <yelite958@gmail.com>
2023-02-02 14:48:47 -0500

    Fix mold wrapper

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,15 +3,17 @@ final: prev:
   elftoolchain = prev.callPackage ./elftoolchain { };
 
   # Wrap mold according the snippet from https://github.com/NixOS/nixpkgs/pull/172452#issuecomment-1335903570
-  mold-wrapped = (prev.wrapBintoolsWith {
+  mold-wrapped = (final.wrapBintoolsWith {
     bintools = final.mold;
   }).overrideAttrs (old: {
     installPhase = old.installPhase + ''
-      for variant in ld.mold ld64.mold; do
+      for variant in ld.mold ld64.mold mold; do
         local underlying=$ldPath/$variant
         [[ -e "$underlying" ]] || continue
         wrap $variant ${prev.path}/pkgs/build-support/bintools-wrapper/ld-wrapper.sh $underlying
       done
+      mkdir -p $out/libexec/mold
+      ln -s $out/bin/ld.mold $out/libexec/mold/ld
     '';
   });
 }
